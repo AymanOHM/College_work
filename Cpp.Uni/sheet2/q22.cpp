9e4127d15d5a06f9cca5277e63cc3af2b15c31cb
AymanOHM <ayman_osama@outlook.com>
2022-10-22 18:07:08 -0400

    didnt understand the question well

--- Cpp.Uni/sheet2/q22.cpp
+++ Cpp.Uni/sheet2/q22.cpp
@@ -4,9 +4,15 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	cout << "the numbers 1 -> 4 : \n";
-	cout << 1 << 2; 
-	cout << " ";
-	cout << 3 << 4;
+	cout << "the numbers 1 -> 4 : " << br;
+	//using one output statment
+	cout << "12 34" << br;
+	//using one output statment with four stream insertion operators
+	cout << "1" << "2 " << "3" << "4\n";
+	//using four output statments
+	cout << "1"; 
+	cout << "2 ";
+	cout << "3"; 
+	cout << "4";
 	return 0;
 }
